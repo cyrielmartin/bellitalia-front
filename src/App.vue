bc6deae6ddf8b9c507840b83c21d8c29f4ca1a10
Cyriel Martin <cyriel.martin@gmail.com>
2019-09-24 21:26:10 +0200

    premier commit sur nouveau repo front

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container-fluid">
+
+    <Map/>    
+
+  </div>
+</template>
+
+<script>
+import Map from './components/Map.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Map
+  }
+}
+</script>
+
+<style>
+
+</style>

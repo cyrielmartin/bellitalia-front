a7163fd7b43bc8b5a2e53bcc91dd9b773612b8c0
Cyriel Martin <cma@skylab-x.io>
2020-02-04 23:04:58 +0100

    options dans les popup ok

--- src/components/InterestMap.vue
+++ src/components/InterestMap.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="">
     <div class="row map">
-      <!-- Affichage de la position réelle -->
-      <!-- <button type="button" name="button" @click="centerMap">Recentrer la carte</button> -->
-      <!-- <small>{{currentCenter}}</small> -->
       <!-- Affichage de la carte -->
       <l-map
       ref="myMap"
@@ -22,7 +19,8 @@
       <!-- Icône pour marqueurs -->
       <l-icon :icon-size="interest.iconSize" :icon-url="icon">
       </l-icon>
-      <l-popup><div><span  :key="interestTag" v-for="(tag, interestTag) in interest.tags" class="badge badge-warning mr-1 popupText">{{tag.name}}</span></div><h1 class="mt-3 mb-3">{{interest.name}}</h1><img :src="interest.image" width="300" class="popupImage"/><div class="badge badge-info popupText"><span><i class="fas fa-location-arrow"></i> {{interest.city.name}}</span>, <span :key="interestRegion" v-for="(region, interestRegion) in interest.city">{{region.name}}</span></div><p class="popupText">{{interest.description}}</p><p><a class="popupText" target="_blank" rel="noopener noreferrer" :href="linkUrl+interest.link"><i class="fas fa-external-link-alt"></i> Lien</a></p><div class="badge badge-secondary popupText"><i class="far fa-calendar"></i> Bell'Italia n°{{interest.bellitalia.number}}, {{interest.bellitalia.publication | moment("MM/YYYY")}}</div><br><div class="mt-4"><a :href="/interest/+interest.id"><i class="far fa-edit"></i> Modifier</a><span class="ml-4 deleteLink" @click="deleteButton($event, interest.id)"><i class="far fa-trash-alt deleteLink"></i> Supprimer</span></div></l-popup>
+            <!-- <l-popup :options="{ autoClose: false, closeOnClick: false }" -->
+      <l-popup :options="{ keepInView: true}"><div><span  :key="interestTag" v-for="(tag, interestTag) in interest.tags" class="badge badge-warning mr-1 popupText">{{tag.name}}</span></div><h1 class="mt-3 mb-3">{{interest.name}}</h1><img :src="interest.image" width="300" class="popupImage"/><div class="badge badge-info popupText"><span><i class="fas fa-location-arrow"></i> {{interest.city.name}}</span>, <span :key="interestRegion" v-for="(region, interestRegion) in interest.city">{{region.name}}</span></div><p class="popupText">{{interest.description}}</p><p><a class="popupText" target="_blank" rel="noopener noreferrer" :href="linkUrl+interest.link"><i class="fas fa-external-link-alt"></i> Lien</a></p><div class="badge badge-secondary popupText"><i class="far fa-calendar"></i> Bell'Italia n°{{interest.bellitalia.number}}, {{interest.bellitalia.publication | moment("MM/YYYY")}}</div><br><div class="mt-4"><a :href="/interest/+interest.id"><i class="far fa-edit"></i> Modifier</a><span class="ml-4 deleteLink" @click="deleteButton($event, interest.id)"><i class="far fa-trash-alt deleteLink"></i> Supprimer</span></div></l-popup>
     </l-marker>
   </l-map>
   <modal name="delete">
@@ -47,8 +45,8 @@ export default {
     return {
       zoom: 5.5,
       maxZoom: 15,
-      center: L.latLng(41.89591, 12.508798),
-      currentCenter: L.latLng(41.89591, 12.508798),
+      center: [41.89591, 12.508798],
+      currentCenter: [41.89591, 12.508798],
       url: 'https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png',
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       marker: L.latLng(41.89591, 12.508798),
@@ -67,9 +65,12 @@ export default {
 
   },
   methods: {
-    // centerMap : function(center) {
-    //   this.center = L.latLng(41.89591, 12.508798)
+    // popupclose: function(){
+    //   console.log('popupclose');
     // },
+    centerUpdate: function(center) {
+      this.currentCenter = center
+    },
     deleteButton(event, id){
       this.$modal.show('dialog', {
         title: 'Suppression d\'un point d\'intérêt',
@@ -102,9 +103,6 @@ export default {
   latLng: function(lat, lng) {
     return L.latLng(lat, lng)
   },
-  centerUpdate: function(center) {
-    this.currentCenter = center
-  }
 },
 props: {
   interests: Array,

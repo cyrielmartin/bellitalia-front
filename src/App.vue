cc1df2e0d6c324332899fb811edaa9e6a1777b67
Cyriel Martin <cma@skylab-x.io>
2020-02-13 13:20:57 +0100

    Responsive banner ok

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,7 @@
   <div class="container-fluid">
     <!-- Header -->
     <div class="row">
-      <a href="/" class="col-6" id="banner"></a>
-        <!-- <a href="/" class="text-center text-info title"><div >Bell'Italia</div></a> -->
-
+      <a href="/" id="banner"></a>
     </div>
     <!-- L'affichage des composants est géré par le router -->
     <router-view></router-view>
@@ -36,31 +34,141 @@ a:hover {
   text-decoration: none;
 }
 
-@media screen and (min-width: 1400px) {
+/* // Small devices (landscape phones, 100px and up) */
+@media (min-width: 100px) {
   #banner{
     background: url('./assets/bellitalia.png') no-repeat center;
-    background-size: 80%;
-    padding-bottom: 10em;
-    margin: auto;
+    background-size: 100%;
+    padding-bottom: 4em;
+    display: inline-block;
+    width: 70%;
   }
 }
 
-@media screen and (min-width: 1850px) {
+/* // Small devices (landscape phones, 300px and up) */
+@media (min-width: 300px) {
   #banner{
     background: url('./assets/bellitalia.png') no-repeat center;
-    background-size: 65%;
-    padding-bottom: 13em;
-    margin: auto;
+    background-size: 100%;
+    padding-bottom: 5em;
+    display: inline-block;
+    width: 65%;
   }
 }
 
-@media screen and (max-width: 1000px) {
+/* // Small devices (landscape phones, 576px and up) */
+@media (min-width: 576px) {
+  #banner{
+    background: url('./assets/bellitalia.png') no-repeat center;
+    background-size: 100%;
+    padding-bottom: 6em;
+    display: inline-block;
+    width: 60%;
+  }
+}
+
+/* // Medium devices (tablets, 768px and up) */
+@media (min-width: 768px) {
   #banner{
     background: url('./assets/bellitalia.png') no-repeat center;
     background-size: 100%;
     padding-bottom: 7em;
-    margin: auto;
+    display: inline-block;
+    width: 55%;
   }
 }
+
+/* // Large devices (desktops, 992px and up) */
+@media (min-width: 992px) {
+  #banner{
+    background: url('./assets/bellitalia.png') no-repeat center;
+    background-size: 100%;
+    padding-bottom: 8em;
+    display: inline-block;
+    width: 50%;
+  }
+}
+
+/* // Extra large devices (large desktops, 1200px and up) */
+@media (min-width: 1200px) {
+  #banner{
+    background: url('./assets/bellitalia.png') no-repeat center;
+    background-size: 100%;
+    padding-bottom: 9em;
+    display: inline-block;
+    width: 45%;
+  }
+}
+
+/* // Extra large devices (large desktops, 1500px and up) */
+@media (min-width: 1500px) {
+  #banner{
+    background: url('./assets/bellitalia.png') no-repeat center;
+    background-size: 100%;
+    padding-bottom: 10em;
+    display: inline-block;
+    width: 40%;
+  }
+}
+
+/* // Extra large devices (large desktops, 1900px and up) */
+@media (min-width: 1900px) {
+  #banner{
+    background: url('./assets/bellitalia.png') no-repeat center;
+    background-size: 100%;
+    padding-bottom: 11em;
+    display: inline-block;
+    width: 35%;
+  }
+}
+
+/* // Extra large devices (large desktops, 2200px and up) */
+@media (min-width: 2200px) {
+  #banner{
+    background: url('./assets/bellitalia.png') no-repeat center;
+    background-size: 100%;
+    padding-bottom: 12em;
+    display: inline-block;
+    width: 30%
+  }
+}
+
+/* // Extra large devices (large desktops, 2500px and up) */
+@media (min-width: 2500px) {
+  #banner{
+    background: url('./assets/bellitalia.png') no-repeat center;
+    background-size: 100%;
+    padding-bottom: 13em;
+    display: inline-block;
+    width: 25%
+  }
+}
+
+/* @media screen and (min-width: 1400px) {
+#banner{
+background: url('./assets/bellitalia.png') no-repeat center;
+background-size: 80%;
+padding-bottom: 10em;
+margin: auto;
+}
+}
+
+@media screen and (min-width: 1850px) {
+#banner{
+background: url('./assets/bellitalia.png') no-repeat center;
+background-size: 65%;
+padding-bottom: 13em;
+margin: auto;
+}
+}
+
+@media screen and (max-width: 1000px) {
+#banner{
+background: url('./assets/bellitalia.png') no-repeat center;
+background-size: 100%;
+padding-bottom: 7em;
+margin: auto;
+}
+} */
 
 </style>

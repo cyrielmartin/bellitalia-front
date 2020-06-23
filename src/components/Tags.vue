a6d42de4d988a68e814b66539c6ef06fef0b0b02
Cyriel Martin <cyriel.martin@gmail.com>
2020-06-23 15:47:31 +0200

    Datatable categories WIP

--- src/components/Tags.vue
+++ src/components/Tags.vue
@@ -5,13 +5,17 @@
       <div class="col-md-10">
         <span class="formTitle">Gérer les catégories</span>
         <div class="card-body">
-          ICI LE DATATABLE
-          <!-- {{tags}} -->
-          <div class="" v-for="tag in tags">
-            Nom du tag {{tag.name}}<br>
-            Créé le {{tag.created_at | moment("DD MMMM YYYY")}}<br>
-            Nombre de points d'intérêt associés {{tag.interests.length}}<br>
-            <br>
+
+          <div class="col-xs-12">
+            <div class="form-group datatableFilterInput">
+              <label for="filter" class="sr-only">Filter</label>
+              <input type="text" class="form-control" v-model="datatableFilter" placeholder="Recherche" @keydown="$event.stopImmediatePropagation()">
+            </div>
+          </div>
+
+          <div class="col-xs-12 table-responsive">
+            <datatable :columns="columns" :data="tags" :filter="datatableFilter" :per-page="25"></datatable>
+            <datatable-pager v-model="datatablePage"></datatable-pager>
           </div>
         </div>
       </div>
@@ -28,6 +32,13 @@ export default {
   data:function(){
     return {
       tags:[],
+      datatableFilter:'',
+      datatablePage: 1,
+      columns: [
+        {label: 'Nom de la catégorie', field: 'name', headerClass: 'class-in-header second-class', align: 'center'},
+        {label: 'Nombre de points d\'intérêt associés', field: 'interests.length', align: 'center'},
+        {label: 'Actions', representedAs: row => `<i class="far fa-edit"></i> <i class="far fa-trash-alt"></i>`, align: 'center', interpolate: true, sortable: false},
+      ],
     }
   },
   mounted: function() {
@@ -47,5 +58,9 @@ export default {
   font-size: 1.6em;
   display: flex;
   justify-content: center;
+}
+.datatableFilterInput{
+  max-width: 14em;
+  margin: auto auto 1em auto;
 }
 </style>
